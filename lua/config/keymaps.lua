70ef20245f6d79caf37c1fa8e684eed7f0bf60f0
Sam Davidge <sam.davidge@onbuy.com>
2023-12-15 14:10:18 +0000

    Use Neotree reveal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 
 -- ctrl + / [Jump to Neotree]
-vim.keymap.set("n", "<C-_>", "<Cmd>Neotree<Cr>")
+vim.keymap.set("n", "<C-_>", "<Cmd>Neotree reveal<Cr>")
 
 -- shift + Right [Next Buffer]
 vim.keymap.set("n", "<S-Right>", ":bnext<Cr>")
