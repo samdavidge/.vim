e46c636005f3ac9d780179a4a13bd4f40ebd9de5
samdavidge <samdavidge93@gmail.comn>
2025-02-01 16:23:11 +0000

    Configure Conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+return {
+  "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
+  opts = function()
+    local opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        php = { "pint" },
+      },
+      formatters = {
+        prettier = {
+          single_quote = true,
+          jsx_single_quote = true,
+        },
+      },
+    }
+    return opts
+  end,
+}
