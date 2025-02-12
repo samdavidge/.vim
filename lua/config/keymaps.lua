e4140e0b2fd41928e3098b934b44adf950b7a789
samdavidge <samdavidge93@gmail.comn>
2025-02-12 21:12:21 +0000

    Get rid of Neotree as snacks is niiiice

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Leader \  [Jump to Neotree]
-vim.keymap.set("n", "<Leader>\\", ":Neotree reveal<Cr>", { silent = true })
-vim.keymap.set("n", "<Leader>s", ":Neotree float git_status<Cr>", { silent = true })
+-- Leader \  [Toggle explorer]
+vim.keymap.set("n", "<Leader>\\", function()
+  Snacks.explorer()
+end, { silent = true })
 
 -- alt + Right [Next Buffer]
 vim.keymap.set("n", "<A-Right>", ":bnext<Cr>", { silent = true })
