67467aaef423a9d9553818fb5271d9b40ca1473a
Sam Davidge <samdavidge93@gmail.com>
2026-04-09 16:49:10 +0100

    Update tdd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,24 +15,6 @@ vim.keymap.set("n", "<Leader>bc", "::BufferCloseAllButPinned<Cr>", { silent = tr
 
 -- Add jumplist and quickfix to telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>j", builtin.jumplist)
-vim.keymap.set("n", "<leader>q", builtin.quickfixhistory)
-
--- pest
-vim.keymap.set("n", "<leader>tr", function()
-  require("tdd").when_test(function(file)
-    -- The command runs your test, then opens a shell so the popup stays open
-    local cwd = vim.fn.getcwd()
-    local popup_cmd = string.format(
-      "tmux popup -w 100%% -h 70%% -E 'bash -c \"cd %s && sail pest %s; echo; echo Press any key to close...; read -n 1\"'",
-      cwd,
-      file
-    )
-    vim.fn.system(popup_cmd)
-  end)
-end, { desc = "Run a test" })
-
-local harpoon = require("harpoon")
 
 harpoon:setup()
 
--- lua/plugins/tdd.lua
+++ lua/plugins/tdd.lua
@@ -1,9 +1,46 @@
 return {
-  lazy = false,
   "aaronlord/tdd.nvim",
+  lazy = false,
   config = function()
+    local tdd = require("tdd")
+
+    tdd.setup({
+      runners = {
+        pest = {
+          command = function(file, test_name)
+            if test_name then
+              return string.format('./vendor/bin/pest pest --stop-on-defect %s --filter "%s"', file, test_name)
+            else
+              return string.format("./vendor/bin/pest pest --stop-on-defect %s", file)
+            end
+          end,
+        },
+        vitest = {
+          command = function(file, _, line_number)
+            if line_number then
+              return string.format("./vendor/bin/pest npm run test:run -- %s:%s", file, line_number)
+            else
+              return string.format("./vendor/bin/pest npm run test:run -- %s", file)
+            end
+          end,
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>tt", function()
+      tdd.jump(true)
+    end, { desc = "Jump to test or SUT" })
+
     vim.keymap.set("n", "<leader>tj", function()
-      require("tdd").jump()
-    end, { desc = "Jump between tests and the sut" })
+      tdd.jump(false)
+    end, { desc = "Goto the SUT or show all test options" })
+
+    vim.keymap.set("n", "<leader>tr", function()
+      tdd.run_test_file()
+    end, { desc = "Run the current test file" })
+
+    vim.keymap.set("n", "<leader><leader>tr", function()
+      tdd.run_test()
+    end, { desc = "Run the current test" })
   end,
 }
